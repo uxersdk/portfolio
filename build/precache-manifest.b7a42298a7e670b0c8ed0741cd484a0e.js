97fd87876b12756ce6a4c860be037f6a644890a7
UXers <kasper@uxers.dk>
2019-05-16 10:38:55 +0200

    Small changes

---
+++ build/precache-manifest.b7a42298a7e670b0c8ed0741cd484a0e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b1363ce857b3c87219b331f167f1340",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97785066eeb7f4cb556c",
+    "url": "/static/css/main.d41dcbd9.chunk.css"
+  },
+  {
+    "revision": "583ddd9d9fab14465b94",
+    "url": "/static/js/2.3623d256.chunk.js"
+  },
+  {
+    "revision": "97785066eeb7f4cb556c",
+    "url": "/static/js/main.2e2c9081.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
